cc4ba91d6e1315786eb1c10ac5c492596476a57e
loualba989-collab <lou.alba989@gmail.com>
2026-05-16 09:53:30 -0500

    Implement file uploader for CSV files
    Added file uploader to allow multiple CSV file uploads and display their contents.

--- app.py
+++ app.py
@@ -5,3 +5,19 @@ st.title("Proyecto UCG")
 st.sidebar.title("Parámetros")
 
 st.sidebar.image("Paython_Logo.png")
+
+import streamlit as st
+import pandas as pd
+
+st.title("Proyecto final UCG")
+
+st.sidebar.title("Parámetros")
+
+st.sidebar.image("Python_logo.png")
+
+uploaded_files = st.file_uploader(
+    "Upload data", accept_multiple_files=True, type="csv"
+)
+for uploaded_file in uploaded_files:
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
