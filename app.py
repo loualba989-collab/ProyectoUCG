2bdaa0385b8b5a699d71cd0bd349245c6457bb4d
loualba989-collab <lou.alba989@gmail.com>
2026-06-06 15:15:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,22 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
+
+st.set_page_config(
+    page_title="Lactancia Materna Ecuador",
+    layout="wide"
+)
+
+st.sidebar.title("Menú")
+
+menu = st.sidebar.radio(
+    "Seleccione una opción",
+    [
+        "Inicio",
+        "Carga de datos",
+        "Exploración",
+        "Calidad de datos",
+        "Visualizaciones",
+        "Conclusiones"
+    ]
+)
